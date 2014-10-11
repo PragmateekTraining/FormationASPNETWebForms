5f1b61322697cd4645ac600c098d6e7992bd0eb0
Pragmateek <training@pragmateek.com>
2014-10-11 23:36:59 +0200

    Add a first simple form sample

---
+++ BasicFormSample/Form.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Form.aspx.cs" Inherits="BasicFormSample.Form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox runat="server" ID="leftInput"></asp:TextBox>
+            <asp:Label runat="server">+</asp:Label>
+            <asp:TextBox runat="server" ID="rightInput"></asp:TextBox>
+            <asp:Button runat="server" Text="Add" OnClick="Button_Click" />
+            <asp:Label runat="server" ID="resultOutput"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ BasicFormSample/Form.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BasicFormSample
+{
+    public partial class Form : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            decimal left = decimal.Parse(leftInput.Text, CultureInfo.InvariantCulture);
+            decimal right = decimal.Parse(rightInput.Text, CultureInfo.InvariantCulture);
+
+            decimal result = left + right;
+
+            resultOutput.Text = result.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
