e48ff0ae64e3880022ea8e443462dbbabd75077f
Pragmateek <training@pragmateek.com>
2014-10-15 21:57:35 +0200

    Add GridView sample for visualizing logs

---
+++ Logging.Client.Web.WebForms/LogsView.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LogsView.aspx.cs" Inherits="Logging.Client.Web.WebForms.LogsView" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <meta http-equiv="refresh" content="5">
+    <style>
+        .log-info {
+            background-color: DarkTurquoise;
+        }
+
+        .log-warning {
+            background-color: DarkOrange;
+        }
+
+        .log-error {
+            background-color: Tomato;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:GridView runat="server" ID="grid"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Logging.Client.Web.WebForms/LogsView.aspx.cs
@@ -0,0 +1,36 @@
+﻿using Logging.Model;
+using Logging.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Logging.Client.Web.WebForms
+{
+    public partial class LogsView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ILogsRepository repository = new LINQtoSQLRepository("prodConnection");
+
+            IEnumerable<Log> logs = repository.GetAllLogs();
+
+            grid.RowDataBound += grid_RowDataBound;
+
+            grid.DataSource = logs;
+            grid.DataBind();
+        }
+
+        void grid_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                Log log = (Log)e.Row.DataItem;
+
+                e.Row.CssClass += "log-" + log.Level.ToString().ToLower();
+            }
+        }
+    }
+}
